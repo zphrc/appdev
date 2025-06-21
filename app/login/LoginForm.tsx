35278c7d137b87af3920104abf33686dca52521d
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-06-21 13:15:52 +0800

    Added the login page

---
+++ app/login/LoginForm.tsx
@@ -0,0 +1,64 @@
+"use client";
+import Link from "next/link";
+import { useRef, ReactNode } from "react";
+
+function InputGroup({
+  label,
+  name,
+  type = "text",
+  inputRef,
+}: {
+  label: string;
+  name: string;
+  type?: string;
+  inputRef: React.RefObject<HTMLInputElement | null>;
+}) {
+  return (
+    <div
+      className="cursor-text rounded-md border-1 px-5 py-3"
+      onClick={() => inputRef.current && inputRef.current.focus()}
+    >
+      <label htmlFor={name} className="cursor-text opacity-60">
+        {label}
+      </label>
+      <br />
+      <input
+        ref={inputRef}
+        id={name}
+        name={name}
+        type={type}
+        className="w-full focus:outline-none"
+      />
+    </div>
+  );
+}
+
+export default function LoginForm() {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  return (
+    <form className="flex flex-col gap-4">
+      <InputGroup label="Email" name="email" type="text" inputRef={emailRef} />
+      <InputGroup
+        label="Password"
+        name="password"
+        type="password"
+        inputRef={passwordRef}
+      />
+      <div className="flex justify-end">
+        <Link
+          href="/forgot-password"
+          className="text-sm text-blue-400 hover:underline"
+        >
+          Forgot Password?
+        </Link>
+      </div>
+      <button
+        type="submit"
+        className="w-full rounded-md bg-white py-2 text-xl font-bold text-black active:bg-gray-400"
+      >
+        Login
+      </button>
+    </form>
+  );
+}
---
+++ app/login/page.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+import LoginForm from "./LoginForm";
+
+export default function Login() {
+  return (
+    <div className="h-screen w-[50vw] bg-black p-10">
+      <div className="flex items-center gap-2 text-white">
+        <Image
+          src="/favicon.ico"
+          alt="Logo"
+          width={50}
+          height={50}
+          className="mb-1 w-5"
+        />
+        <div className="text-xl font-bold">DevMate</div>
+      </div>
+      <div className="flex flex-col gap-6 p-10">
+        <div>
+          <div className="mb-3 text-3xl font-bold">Welcome back!</div>
+          <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+        </div>
+        <Link
+          href=""
+          className="rounded-md border-2 py-2 text-center text-lg font-bold"
+        >
+          Continue with Google
+        </Link>
+        <div className="text-center uppercase">or</div>
+        <LoginForm /> {/* This is a Client Component */}
+        <div className="text-center">
+          Don't have an account? <span className="text-blue-400">Sign up</span>!
+        </div>
+      </div>
+    </div>
+  );
+}
