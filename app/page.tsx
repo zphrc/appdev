d4cbb131d2447addf816dd71bd5008ae16ac67f7
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-06-21 19:39:33 +0800

    Updated page.tsx
    Reduced the size of the text.

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ export default function Home() {
   return (
     <>
       <div className="flex h-screen w-screen flex-col justify-center gap-10 text-center">
-        <div className="text-7xl font-bold">Your ultimate study buddy.</div>
-        <div className="text-2xl font-light">
+        <div className="text-6xl font-bold">Your ultimate study buddy.</div>
+        <div className="text-xl font-light">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         </div>
         <div>
           <Link
-            href="#"
-            className="rounded-md bg-(--foreground) px-13 py-4 text-2xl font-bold text-(--background)"
+            href=""
+            className="rounded-md bg-white px-13 py-4 text-xl font-bold text-black"
           >
             Get Started
           </Link>
