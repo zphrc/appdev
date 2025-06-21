4b85b58547ef7a8b3e59cd1b3a8b62f557ecf927
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-06-21 22:46:59 +0800

    Updated layout.tsx
    Added a navigation bar.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import type { Metadata } from "next";
+import Link from "next/link";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -15,6 +16,41 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="antialiased">
+        <nav className="m-7 flex justify-around">
+          <Link href="/" className="flex items-center gap-2">
+            <Image
+              src="/favicon.ico"
+              alt="DevMate Logo"
+              width={25}
+              height={25}
+              className="rounded-full"
+            />
+            <span className="text-3xl font-bold">DevMate</span>
+          </Link>
+          <div className="flex items-center gap-10 *:font-bold">
+            <Link href="/" className="capitalize">
+              home
+            </Link>
+            <Link href="/features" className="capitalize">
+              features
+            </Link>
+            <Link href="/how-it-works" className="capitalize">
+              how it works
+            </Link>
+            <Link href="/tracks" className="capitalize">
+              tracks
+            </Link>
+            <Link href="/faq" className="uppercase">
+              faq
+            </Link>
+          </div>
+          <Link
+            href="/login"
+            className="block rounded-md border-2 px-5 py-1 font-bold"
+          >
+            Login
+          </Link>
+        </nav>
         <Image
           src="/home_background.png"
           alt="Home page background"
