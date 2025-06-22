f28da79cd1f75e55ccfcb192aa454942ac4dfad1
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-06-22 21:56:59 +0800

    Updated page.tsx
    Removed next/image Image import

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,41 +1,32 @@
-import Image from "next/image";
 import Link from "next/link";
 import { FcGoogle } from "react-icons/fc";
 import LoginForm from "./LoginForm";
 
 export default function Login() {
   return (
-    <div className="h-screen w-[50vw] bg-black p-10">
-      <div className="flex items-center gap-2 text-white">
-        <Image
-          src="/favicon.ico"
-          alt="Logo"
-          width={50}
-          height={50}
-          className="mb-1 w-5"
-        />
-        <div className="text-xl font-bold">DevMate</div>
-      </div>
-      <div className="flex flex-col gap-6 p-10">
+    <div className="flex h-[80vh] w-full justify-center px-10">
+      <div className="grid w-[90vw] grid-cols-2 items-center gap-6 border-2 bg-black p-10">
         <div>
           <div className="mb-3 text-3xl font-bold">Welcome back!</div>
           <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
         </div>
-        <Link
-          href=""
-          className="rounded-md border-2 py-2 text-center text-lg font-bold"
-        >
-          <FcGoogle className="mr-2 inline-block" />
-          Continue with Google
-        </Link>
-        <div className="text-center uppercase">or</div>
-        <LoginForm /> {/* This is a Client Component */}
-        <div className="text-center">
-          Don&apos;t have an account?{" "}
-          <Link href="/sign-up" className="text-blue-400">
-            Sign up
+        <div>
+          <h2 className="mb-3 text-3xl font-bold">Login</h2>
+          <Link
+            href=""
+            className="block w-full rounded-md border-2 py-2 text-center text-lg font-bold"
+          >
+            <FcGoogle className="mr-2 inline-block" />
+            Continue with Google
           </Link>
-          !
+          <LoginForm /> {/* This is a Client Component */}
+          <div className="text-center">
+            Don&apos;t have an account?{" "}
+            <Link href="/sign-up" className="text-blue-400">
+              Sign up
+            </Link>
+            !
+          </div>
         </div>
       </div>
     </div>
