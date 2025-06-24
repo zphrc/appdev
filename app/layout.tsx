33c05093a618afe2f6506c6074f53ef1d1ea7544
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-06-24 11:36:07 +0800

    Updated layout.tsx
    The style "*:font-bold" is now transferred to the main <nav> tag instead of each of its children (except the Image).

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="antialiased">
-        <nav className="m-7 flex flex-wrap justify-around">
+        <nav className="m-7 flex flex-wrap justify-around *:font-bold">
           <Link href="/" className="flex items-center gap-2">
             <Image
               src="/favicon.ico"
@@ -25,9 +25,9 @@ export default function RootLayout({
               height={25}
               className="rounded-full"
             />
-            <span className="text-3xl font-bold">DevMate</span>
+            <span className="text-3xl">DevMate</span>
           </Link>
-          <div className="flex flex-wrap items-center gap-10 *:font-bold">
+          <div className="flex flex-wrap items-center gap-10">
             <Link href="/" className="capitalize">
               home
             </Link>
@@ -44,10 +44,7 @@ export default function RootLayout({
               faq
             </Link>
           </div>
-          <Link
-            href="/login"
-            className="block rounded-md border-2 px-5 py-1 font-bold"
-          >
+          <Link href="/login" className="block rounded-md border-2 px-5 py-1">
             Login
           </Link>
         </nav>
